a7500ae5a31e7bcf22e4b6205d6c938474a619a1
Roman Alexander <romanalexander@users.noreply.github.com>
2014-05-27 16:52:24 -0700

    Updated routes spacing.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,31 +6,30 @@
         'ngAnimate',
         'ngResource',
         'ngSanitize'
-    ])
-        .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-            $locationProvider.html5Mode(true);
-            $urlRouterProvider.otherwise('/');
-            $stateProvider
-                .state('root', {
-                    url: '',
-                    abstract: true,
+    ]).config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+        $locationProvider.html5Mode(true);
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('root', {
+                url: '',
+                abstract: true,
 
-                    views: {
-                        'header': {
-                            templateUrl: 'views/header.html'
-                        },
-                        'footer': {
-                            templateUrl: 'views/footer.html'
-                        }
+                views: {
+                    'header': {
+                        templateUrl: 'views/header.html'
+                    },
+                    'footer': {
+                        templateUrl: 'views/footer.html'
                     }
-                })
-                .state('root.home', {
-                    url: '/',
-                    views: {
-                        'container@': {
-                            templateUrl: 'views/home.html'
-                        }
+                }
+            })
+            .state('root.home', {
+                url: '/',
+                views: {
+                    'container@': {
+                        templateUrl: 'views/home.html'
                     }
-                });
-        }]);
+                }
+            });
+    }]);
 })();
