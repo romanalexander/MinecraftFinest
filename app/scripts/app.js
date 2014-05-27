8ae944a9adfe2f4df29c475763fb7b1ea911de3a
Roman Alexander <romanalexander@users.noreply.github.com>
2014-05-27 13:34:24 -0700

    Updated to have routes.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,27 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular
-  .module('minecraftFinestApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    angular.module('minecraftFinestApp', [
+        'ui.router',
+        'ngAnimate',
+        'ngResource',
+        'ngSanitize'
+    ])
+        .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+            $urlRouterProvider.otherwise('/');
+            $stateProvider
+                .state('root', {
+                    url: '',
+                    abstract: true,
+                    views: {
+                        'header': {
+                            templateUrl: 'static/templates/header.html',
+                            controller: 'HeaderController'
+                        },
+                        'footer': {
+                            templateUrl: 'static/templates/footer.html'
+                        }
+                    }
+                });
+        }]);
+})();
